a93b3d8fe4e8edec35e608fac16ce6d3ca38d670
balramswainn <balramswain789@gmail.com>
2026-03-21 15:40:41 +0530

    deleted the DELETE folder

---
+++ frontend/09redux_main/src/features/todoSlice.js
@@ -0,0 +1,39 @@
+import {createSlice, nanoid } from '@reduxjs/toolkit';
+
+
+const initialState = {
+     todos: JSON.parse(localStorage.getItem('todos')) || [{id: 1, text: "Hello world", completed: false}],
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',                  
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {   
+            const todo = {
+                id: nanoid(), 
+                text: action.payload.text,
+                completed: action.payload.completed
+            }
+            state.todos.push(todo)   
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload ) 
+        },
+         updateTodo:(state,action)=>{
+            state.todos=state.todos.map((todo)=> todo.id === action.payload.id ? {...todo,text:action.payload.text} : todo)
+        },
+        toggleComplete:(state,action)=>{
+            state.todos=state.todos.map((todo)=>todo.id === action.payload ? {...todo, completed :!todo.completed} :todo)
+        }
+       
+    }
+})
+
+
+
+export const {addTodo, removeTodo,updateTodo,toggleComplete} = todoSlice.actions  
+
+export default todoSlice.reducer
--- frontend/Delete/freshmart/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from './CartSlice'
-
-const store=configureStore({
-  reducer:{
-  cart:cartSlice  
-  }
-})
-export default store
