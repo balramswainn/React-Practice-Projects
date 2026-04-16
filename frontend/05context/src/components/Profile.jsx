9d6f169f4c24612dcbdde94c3ea2618edcf16890
balramswainn <balramswain789@gmail.com>
2026-04-16 18:50:57 +0530

    changes

--- frontend/05context/src/components/Profile.jsx
+++ frontend/05context/src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
     
     if (!user) return <div>please login</div>
 
-    return <div>Welcome {user.username} and password is:{user.password}</div>
+    return <div>Welcomee {user.username} and password is:{user.password}</div>
 }
 
 export default Profile
